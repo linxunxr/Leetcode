51f21938a46c82cd9daa69050f99cd2d475f6989
linxun <liulinxr@foxmail.com>
2024-05-25 06:56:45 +0800

    两两交换链表中的节点

--- leetcode/editor/cn/[24]两两交换链表中的节点.cpp
+++ leetcode/editor/cn/[24]两两交换链表中的节点.cpp
@@ -14,7 +14,25 @@ using namespace std;
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-
+        // 设置虚拟头结点用以方便交换
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+        ListNode* cur = dummyHead;
+        // 遍历
+        while (cur->next != nullptr && cur->next->next != nullptr) {
+            // 两个临时节点
+            ListNode* tmp = cur->next;
+            ListNode* tmp1 = cur->next->next->next;
+            // 交换
+            cur->next = cur->next->next;
+            cur->next->next = tmp;
+            cur->next->next->next = tmp1;
+            // 移动，准备下一轮交换
+            cur = cur->next->next;
+        }
+        ListNode* result = dummyHead->next;
+        delete dummyHead;
+        return result;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
